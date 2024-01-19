4556ba58bdf942a9a908db7c73c4212e1fb0e981
Sudipta Mohanty <sudiptadiya20@gmail.com>
2024-01-19 18:41:52 +0530

    Merge remote-tracking branch 'origin/main'

--- test_RegLogin.py
+++ test_RegLogin.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from conftest import driver
+from conftest import user_data
 
 
 #Validating the title of the webpage
@@ -10,7 +11,11 @@ def test_1(test_nav):
 def test_2():
     driver.find_element(By.XPATH, "//a[contains(text(),'home')]").click()
 
-#Navigating to the Registration page
+#Navigating to the Registration page and registering as a user
 def test_3():
     driver.find_element(By.XPATH,"//a[normalize-space()='Register']").click()
+    driver.find_element(By.ID,"name").send_keys(user_data["name"])
+
+
+
 
