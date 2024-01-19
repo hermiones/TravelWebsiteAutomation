572ee75bb107d11b9f7c1395dd606e8a07f2ba2b
Sudipta Mohanty <sudiptadiya20@gmail.com>
2024-01-19 18:20:23 +0530

    Merge remote-tracking branch 'origin/main'

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from selenium import webdriver
-driver = webdriver.Chrome()
-
-@pytest.fixture
-def test_nav():
-    driver.get('https://blazedemo.com/')
--- test_RegLogin.py
+++ test_RegLogin.py
@@ -1,9 +1,16 @@
-import pytest
 from selenium.webdriver.common.by import By
-import conftest
 from conftest import driver
 
 
-
+#Validating the title of the webpage
 def test_1(test_nav):
+    assert driver.title == "BlazeDemo"
+
+#Navigating to the homepage
+def test_2():
     driver.find_element(By.XPATH, "//a[contains(text(),'home')]").click()
+
+#Navigating to the Registration page
+def test_3():
+    driver.find_element(By.XPATH,"//a[normalize-space()='Register']").click()
+
