db210f4d61eca94cd89ebd5b5e990ed066c54230
Sudipta Mohanty <sudiptadiya20@gmail.com>
2024-02-27 17:38:25 +0530

    Merge remote-tracking branch 'origin/main'

--- Testcases/test_FlightBooking.py
+++ Testcases/test_FlightBooking.py
@@ -1,10 +1,30 @@
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from conftest import *
-#book a flight from Boston to rome
+#book a flight from Boston to Paris
 def test_FB1(test_nav):
-    #Selecting the
+    driver.maximize_window()
+    dropdown1 = Select(driver.find_element(By.XPATH,"/html[1]/body[1]/div[3]/form[1]/select[1]"))
+    dropdown1.select_by_visible_text("Boston")
+    dropdown2 = Select(driver.find_element(By.XPATH,"/html[1]/body[1]/div[3]/form[1]/select[2]"))
+    dropdown2.select_by_visible_text("London")
+    driver.find_element(By.XPATH,"/html[1]/body[1]/div[3]/form[1]/div[1]/input[1]").click()
+    try:
+        if "Flights from Boston to London" in driver.page_source:
+            print("The text 'Flights from Boston to London' is present in the page source.")
+        else:
+            print("The text 'Flights from Boston to London' is not present in the page source.")
+
+    except:
+        print("An error occurred:")
+
+
+
+#book a flight from Paris to Paris
+def test_FB2(test_nav):
     dropdown = Select(driver.find_element(By.XPATH,"/html[1]/body[1]/div[3]/form[1]/select[1]"))
     dropdown.select_by_visible_text("Boston")
 
