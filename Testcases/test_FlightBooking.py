090ca37e9b971fe2ec2a313685be8dbdb3fcc0c5
Sudipta Mohanty <sudiptadiya20@gmail.com>
2024-02-28 14:34:00 +0530

    Merge remote-tracking branch 'origin/main'

--- Testcases/test_FlightBooking.py
+++ Testcases/test_FlightBooking.py
@@ -4,7 +4,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 from conftest import *
-#book a flight from Boston to Paris
+
+
+
+
+#select a flight from Boston to Paris
 def test_FB1(test_nav):
     driver.maximize_window()
     dropdown1 = Select(driver.find_element(By.XPATH,"/html[1]/body[1]/div[3]/form[1]/select[1]"))
@@ -12,17 +16,17 @@ def test_FB1(test_nav):
     dropdown2 = Select(driver.find_element(By.XPATH,"/html[1]/body[1]/div[3]/form[1]/select[2]"))
     dropdown2.select_by_visible_text("London")
     driver.find_element(By.XPATH,"/html[1]/body[1]/div[3]/form[1]/div[1]/input[1]").click()
-    try:
-        if "Flights from Boston to London" in driver.page_source:
-            print("The text 'Flights from Boston to London' is present in the page source.")
-        else:
-            print("The text 'Flights from Boston to London' is not present in the page source.")
 
-    except:
-        print("An error occurred:")
+#Validate that the search results are coming for boston to paris
+def test_FB2():
+    assert "Flights from Boston to London" in driver.page_source
 
+#validate that the user is in the purchase page
+def test_FB3():
+    driver.find_element(By.XPATH,"/html[1]/body[1]/div[2]/table[1]/tbody[1]/tr[3]/td[1]/input[1]").click()
+    assert "BlazeDemo Purchase" in driver.title
 
-
-
-
-
+def test_FB4():
+    driver.find_element(By.ID,"inputName").send_keys(user_data["name"])
+#will skip the part where will be adding user details and card details for now
+    driver.find_element(By.XPATH,"/html[1]/body[1]/div[2]/form[1]/div[11]/div[1]/input[1]").click()
